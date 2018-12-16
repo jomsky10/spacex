36fb7e2e8ec3427a12ceac6e6a7abed2d3f89867
Jomar Diamse <jomar.diamse@yahoo.com>
2018-12-16 20:11:30 +0800

    spacex test
    spacex test

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// function video_out() {
+//     $('.fullscreen-bg__video').bind('ended',function (){
+//         // $(this).parent().fadeOut();
+//         $('.fullscreen-bg__video').css('opacity','0'); //WILL CHANGE THE PROPERTY OF BIDYO
+//      });
+//   }
+
+  function load() {
+    // $(".loadme").slice(0, 1).show();
+    // $(".button").on('click', function (e) {
+    //     e.preventDefault();
+    //     $("div:hidden").slice(0, 1).slideDown();
+    //     if ($("div:hidden").length == 0) {
+    //         $("#load").fadeOut('slow');
+    //         $(".button").hide(); 
+    //     }
+    //     // $('html,body').animate({
+    //     //     scrollTop: $(this).offset().top
+    //     // }, 1500);
+    // });
+  }
